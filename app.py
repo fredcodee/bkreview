53ca82ad32dbeefe4ed4b385449ec643e390ef64
fredcode <fredcode23@gmail.com>
2020-02-25 18:44:10 +0800

    integreted heroku postgrees

--- app.py
+++
@@ -1,6 +0,0 @@
-from flask import Flask, render_template, request, session
-from flask_session import Session
-
-
-"""goodreads_apikey = {key: 82F9vHtuZ1UxuDWQHifAA,
-             secret: y5zH6y2MYeQp5FCPIP4HTsovGAvsAkmRutHgbYT4Ks}"""
