aa40f965ef68c328cba966e7d540fef75c0e80e3
fredcode <fredcode23@gmail.com>
2020-02-29 04:26:35 +0800

    book data and goodreads api

--- main.py
+++ main.py
@@ -16,15 +16,40 @@ def home():
 def search():
     get_book = request.form.get("book").title()
 
-  #check if book details was provided
+    #check if book details was provided
     if get_book:
-        try:
-            from sqlalchemy import or_
-            books = Books.query.filter(or_(Books.title==get_book, Books.auther==get_book)).all()
+        from sqlalchemy import or_
+        books = Books.query.filter(or_(Books.isbn== get_book, Books.author == get_book, Books.title == get_book)).all()
+        if books:
             return(render_template("booklist.html", books=books))
-        except:
-            flash("error in db code")
+        else:
+            flash("we can't find books with that description.")
             return(redirect(url_for("main.home")))
     else:
         flash("sorry you must provide a book details")
         return(redirect(url_for("main.home")))
+
+
+@main.route("/book/<isbn>")
+def book(isbn):
+    book_id = Books.query.filter_by(isbn=isbn).first()
+
+    """GOODREADS"""
+    #get api key
+    key = "82F9vHtuZ1UxuDWQHifAA"
+    res = requests.get("https://www.goodreads.com/book/review_counts.json",params={"key": key, "isbns": isbn})
+    
+    #parse json to dict
+    a = res.json()
+    bd = a['books'][0]
+
+    # Fetch book reviews
+    book_reviews=""
+
+
+    return(render_template('bookpage.html', title=book_id.title, author=book_id.author, year=book_id.year, isbn=bd['isbn'], av=bd['average_rating'], wrc=bd['work_ratings_count']))
+
+@main.route("/comment", methods=['GET', 'POST'])
+@login_required
+def comment():
+    pass
--- models.py
+++ models.py
@@ -8,7 +8,7 @@ class User(UserMixin, db.Model):
   email = db.Column(db.String(120), unique=True, nullable=False)
   username = db.Column(db.String(80), unique=True, nullable=False)
   password = db.Column(db.String(100))
-  reviews = db.relationship('Review', backref='comment', lazy=True)
+  reviews = db.relationship('Review', backref='feedback', lazy=True)
 
 
 class Books(db.Model):
